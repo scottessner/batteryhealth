0afb1136974ff31f5d99f1efb5119e815d834533
sjessn <scott.j.essner@monsanto.com>
2017-01-11 16:50:52 -0600

    Added requirements.txt and hacked model

--- battery/models.py
+++ battery/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+
 class Battery(models.Model):
-    
+    nickname = models.CharField(max_length=200)
+    capacity = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
